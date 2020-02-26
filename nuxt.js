8df952c093eaf68d182c41be351507ea9e51ce68
jeffjing <zgayjjf@qq.com>
2020-02-26 10:08:02 +0800

    feat: mute nuxt/no-cjs-in-config

--- nuxt.js
+++ nuxt.js
@@ -3,5 +3,9 @@ module.exports = {
     'plugin:nuxt/recommended',
 
     './vue.js'
-  ]
+  ],
+  rules: {
+    // we use nuxt.config.js instead of nuxt.config.ts, thus we can only use cjs for now
+    'nuxt/no-cjs-in-config': [0]
+  }
 }
