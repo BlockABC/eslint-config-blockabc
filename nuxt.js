e90622ab7d07595ccec140832e0e8839e010c7e8
jeffjing <zgayjjf@qq.com>
2021-07-15 20:03:26 +0800

    feat: remove custom rule `nuxt/no-cjs-in-config`

--- nuxt.js
+++ nuxt.js
@@ -4,7 +4,5 @@ module.exports = {
     'plugin:nuxt/recommended',
   ],
   rules: {
-    // we use nuxt.config.js instead of nuxt.config.ts, thus we can only use cjs for now
-    'nuxt/no-cjs-in-config': [0]
   }
 }
