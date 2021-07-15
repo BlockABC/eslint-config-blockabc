90396ff8b3608c702c3cc94426dea96dd0ed7344
jeffjing <zgayjjf@qq.com>
2021-07-15 17:20:52 +0800

    fix: fix nuxt-ts parser bug

--- nuxt-ts.js
+++ nuxt-ts.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: [
     './nuxt.js',
     './vue-ts.js'
-  ]
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
 }
